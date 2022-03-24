96d54c5c181f4eab45588a42685b38e7c80cee51
dtonavitor <vitor.barros@sou.inteli.edu.br>
2022-03-24 14:39:16 -0300

    update minigame ES e criação cena final
    Agora, o minigame de engenharia de software possui timer e pontuação.
    
    Foi criada uma cena final com um gráfico das pontuações por curso e possíveis carreiras para tais.

--- src/scripts/globals.gd
+++ src/scripts/globals.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Node
 
 # Essas variáveis são utilizadas em outros scripts e partes do jogo
 
@@ -12,3 +12,15 @@ var iButton: int = 0
 var button: int = 0
 
 var time: int = 180
+
+var objCatched = false
+var nodes = ["/root/Node2D/Pos1",
+	"/root/Node2D/Pos2",
+	"/root/Node2D/Pos3",
+	"/root/Node2D/Pos4",
+	"/root/Node2D/Pos5"
+]
+
+var nodeNumber = 0
+var nodeNumberMax = len(nodes) - 1
+var correctOrder = true
--- src/scripts/resultES.gd
+++ src/scripts/resultES.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
 func show_result():
-	if Global.correctOrder == true:
+	if Globals.correctOrder == true:
 		$Label.text = "Parabéns!"
 	else:
 		$Label.text = "Errou!"
+
+
+func _on_Button9_pressed():
+	var _changeScene = get_tree().change_scene("res://scenes/bunker.tscn")
