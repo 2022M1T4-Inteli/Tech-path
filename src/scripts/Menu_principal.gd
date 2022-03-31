b6f9d159f3da9cfc8a49f715080a9ef9435969bd
felipesilber <felipe.silberberg@sou.inteli.edu.br>
2022-03-31 09:41:32 -0300

    Novo menu
    Imagens do menu, botão, script e cena

---
+++ src/scripts/Menu_principal.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+
+
+func _on_Jogar_pressed():
+	get_tree().change_scene("res://scenes/nivel.tscn")
+
+
+
+
+
+
+func _on_Som_pressed():
+	pass
+	
+	
+
+
+func _on_Sair_pressed():
+	get_tree().quit()
