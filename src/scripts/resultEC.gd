e5b4f180d6a28efd46cb426732056163ace9418b
dtonavitor <vitor.barros@sou.inteli.edu.br>
2022-04-07 12:40:36 -0300

    correção de bug de arrastar
    agora o jogador pode jogar o minigame novamente

--- src/scripts/resultEC.gd
+++ src/scripts/resultEC.gd
@@ -6,3 +6,13 @@ func show_result():
 		get_tree().change_scene("res://scenes/winEC.tscn")
 	else:
 		get_tree().change_scene("res://scenes/loseEC.tscn")
+	Globals.objCatchedEC = false
+	Globals.nodesEC = ["/root/Node2D/Pos1",
+		"/root/Node2D/Pos2",
+		"/root/Node2D/Pos3",
+		"/root/Node2D/Pos4",
+	]
+
+	Globals.nodeNumberEC = 0
+	Globals.nodeNumberMaxEC = len(Globals.nodesEC) - 1
+	Globals.correctOrderEC = true
