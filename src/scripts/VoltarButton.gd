c361f62e7f741088e5b67f6fbeeefca344c73a11
jordan-acs <jordan.silva@sou.inteli.edu.br>
2022-04-08 11:22:05 -0300

    script que faltou do botão "tentar novamente"

--- src/scripts/VoltarButton.gd
+++ src/scripts/VoltarButton.gd
@@ -3,3 +3,16 @@ extends Button
 # Apertando o botão ocorre mudança de cena
 func _on_VoltarButton_pressed():
 	var _changeScene = get_tree().change_scene("res://scenes/bunker.tscn")
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://scenes/minigame_ciencia_computacao.tscn")
+
+
+func _on_reset_pressed():
+	get_tree().change_scene("res://scenes/minigame_ES.tscn")
+	Globals.nodeNumber = 0
+	Globals.correctOrder = true
+	Globals.onBar = false
+	Globals.inFinalPos = false
+	Globals.inBar = false
